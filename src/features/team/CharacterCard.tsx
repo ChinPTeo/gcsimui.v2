63402aad326b114542386103a00078c1a4f9c5fd
Maryjane Namya <maryjanen.work@gmail.com>
2022-01-22 00:18:14 +0530

    Added UI elements sans implementation

--- src/features/team/CharacterCard.tsx
+++ src/features/team/CharacterCard.tsx
@@ -65,7 +65,7 @@ export default function CharacterCard({ char, index }: CharacterCardProps) {
   //Setting constellation img URLs
   let charURLName = char.name.toLowerCase().split(" ").join("");
   for(let i=0; i<6; i++) {
-    consImages[i] = `public/images/avatar/cons/${charURLName}_c${i+1}.png`;
+    consImages[i] = `/images/avatar/cons/${charURLName}_c${i+1}.png`;
   }
 
   //Artifact logic
@@ -129,67 +129,13 @@ export default function CharacterCard({ char, index }: CharacterCardProps) {
         isOpen={editChar}
         onClose={()=>setEditChar(false)}
         hasBackdrop={editChar}
-        className="bg-emerald-600"
         style={{background: Colors.DARK_GRAY4}}
       >
-        <div className="p-2">
-          <div>
-            <EditCharacterModal char={char} />
-          </div>
+        <div className="p-4 text-white ">
+          <EditCharacterModal char={char} consImg={consImages} />
         </div>
       </Dialog>
-      {/* {editChar && (
-        <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Modal Title</h3>
-                  <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setEditChar(false)}
-                  >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                  </button>
-                </div>
-                
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
-                </div>
-                
-                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setEditChar(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setEditChar(false)}
-                  >
-                    Save Changes
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      )} */}
-
+      
       <div className="min-h-48 bg-gray-700 shadow rounded-md text-sm flex flex-col p-2 justify-center gap-2">
         <CharHeader char={char} handleEdit={EditCharacter} />
 
--- src/features/team/EditCharacterModal.tsx
+++ src/features/team/EditCharacterModal.tsx
@@ -1,17 +1,291 @@
-import React from "react";
+import React, { useState } from "react";
 import { 
-    Character
+    Character,
+    ascLvlMin,
+    charAscLvlCap
  } from '../../util';
 
 type EditCharacterProps = {
     char: Character;
+    consImg: string[];
 }
 
-export default function EditCharacterModal({char}: EditCharacterProps) {
+export default function EditCharacterModal({char, consImg}: EditCharacterProps) {
 
+    let lvl = char.level ? char.level : 0;
+    let asc = char.ascension ? charAscLvlCap(char.ascension) : 0;
+    let cons = char.constellation ? char.constellation : 0;
+    let tal = {
+        auto: char.talent.auto ? char.talent.auto : 1,
+        skill: char.talent.skill ? char.talent.skill : 1,
+        burst: char.talent.burst ? char.talent.burst : 1,
+    };
+
+    const [minLvl, setMinLvl] = useState(ascLvlMin(asc));
+    const [maxLvl, setMaxLvl] = useState(charAscLvlCap(asc));
+    
     return(
-        <div>
-            <p>Modal Test</p>
-        </div>
+        <>
+            <div className="font-medium text-lg rounded-md mb-2">
+                {char.name}
+            </div>
+            <div className="ml-2">
+                <div className="flex flex-col">
+                    <div className="flex flex-row items-center">
+                        <span className="flex-grow">Level </span>
+                        <div className="flex flex-row">
+                            {Array(6).fill(0).map((e, i) => {
+                                return(
+                                    <span
+                                        key={i}
+                                        className={i < asc
+                                            ? "fill-current text-yellow-400 hover:bg-gray-500 p-1 rounded-md"
+                                            : "hover:bg-gray-500 p-1 rounded-md"}
+                                        onClick={() => {
+                                            asc = asc == i + 1 ? i : i + 1;
+                                            const min = ascLvlMin(asc);
+                                            const max = charAscLvlCap(asc);
+                                            if(lvl > max) {
+                                                lvl = max;
+                                                console.log("lvl set to: ", lvl);
+                                            }
+                                            if(lvl < min) {
+                                                lvl = min;
+                                                console.log("lvl set to: ", lvl);
+                                            }
+                                            //handleChange();
+                                        }}
+                                    >
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="h-7 w-7"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                                            />
+                                        </svg>
+                                    </span>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+                <label>
+                    <input 
+                        type="range"
+                        className="range p-1 mt-2"
+                        min={minLvl}
+                        max={maxLvl}
+                        value={lvl}
+                        onInput={(e) => {
+                            //lvl = parseInt(e.target.value)
+                            //handleChange();
+                        }}
+                        onChange={(e) => {
+                            //lvl = parseInt(e.target.value)
+                            //handleChange();
+                        }}
+                    />
+                </label>
+            </div>
+            <div className="flex flex-col mt-4">
+                <div className="grid grid-cols-6 gap-2">
+                    {consImg.map((url, i) => {
+                        //console.log(url)
+                        return(
+                            <div
+                                className={cons > i
+                                ? "rounded-md border-2 border-gray-100 opacity-100 hover:bg-gray-500"
+                                : "rounded-md border-2 border-transparent opacity-25 hover:bg-gray-500"}
+                                onClick={() => {
+                                    cons = cons == i + 1 ? i : i + 1;
+                                    //handleChange();
+                                }}
+                            >
+                                <img src={url} alt="" className="w-12 h-12"   />                              
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="grid grid-cols-3 mt-4">
+                    <div className="flex flex-col ml-2 mt-1 items-center">
+                        <span>Normal</span>
+                        <div className="flex flex-row items-center">
+                            <div className="flex flex-col justify-center text-center rounded-full bg-gray-600 h-10 w-10">
+                                {tal.auto}
+                            </div>
+                            <div className="flex flex-col">
+                                <div
+                                    className="rounded-full hover:bg-gray-600"
+                                    onClick={() => {
+                                        if(tal.auto < 10) {
+                                            tal.auto++;
+                                        }
+                                        //handleChange();
+                                    }}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M5 15l7-7 7 7"
+                                        />
+                                    </svg>
+                                </div>
+                                <div
+                                    className="rounded-full hover:bg-gray-600"
+                                    onClick={() => {
+                                        if(tal.auto > 0) {
+                                            tal.auto--;
+                                        }
+                                        //handleChange();
+                                    }}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M19 9l-7 7-7-7"
+                                        />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col ml-2 mt-1 items-center">
+                        <span>Skill</span>
+                        <div className="flex flex-row items-center">
+                            <div className="flex flex-col justify-center text-center rounded-full bg-gray-600 h-10 w-10">
+                                {tal.skill}
+                            </div>
+                            <div className="flex flex-col">
+                                <div
+                                    className="rounded-full hover:bg-gray-600"
+                                    onClick={() => {
+                                        if(tal.skill < 10) {
+                                            tal.skill++;
+                                        }
+                                        //handleChange();
+                                    }}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M5 15l7-7 7 7"
+                                        />
+                                    </svg>
+                                </div>
+                                <div
+                                    className="rounded-full hover:bg-gray-600"
+                                    onClick={() => {
+                                        if(tal.skill > 0) {
+                                            tal.skill--;
+                                        }
+                                        //handleChange();
+                                    }}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M19 9l-7 7-7-7"
+                                        />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col ml-2 mt-1 items-center">
+                        <span>Burst</span>
+                        <div className="flex flex-row items-center">
+                            <div className="flex flex-col justify-center text-center rounded-full bg-gray-600 h-10 w-10">
+                                {tal.burst}
+                            </div>
+                            <div className="flex flex-col">
+                                <div
+                                    className="rounded-full hover:bg-gray-600"
+                                    onClick={() => {
+                                        if(tal.burst < 10) {
+                                            tal.burst++;
+                                        }
+                                        //handleChange();
+                                    }}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M5 15l7-7 7 7"
+                                        />
+                                    </svg>
+                                </div>
+                                <div
+                                    className="rounded-full hover:bg-gray-600"
+                                    onClick={() => {
+                                        if(tal.burst > 0) {
+                                            tal.burst--;
+                                        }
+                                        //handleChange();
+                                    }}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M19 9l-7 7-7-7"
+                                        />
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
